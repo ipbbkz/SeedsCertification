95816508573af723d4fd64bd156950719ebda8ef
Andrey K <kant2002@gmail.com>
2023-10-14 18:02:48 +0000

    Merged PR 8718: Add database structure
    There not relationships yet, not seed data.
    I think what we have now is enough to start working on workflow for farmers

---
+++ СертификацияСемян/Данные/Migrations/20231014145234_СтруктураБД.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace СертификацияСемян.Данные.Migrations
+{
+    /// <inheritdoc />
+    public partial class СтруктураБД : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Анализы",
+                columns: table => new
+                {
+                    Ид = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ИнспекцияИд = table.Column<int>(type: "int", nullable: false),
+                    ТипАнализа = table.Column<int>(type: "int", nullable: false),
+                    Статус = table.Column<int>(type: "int", nullable: false),
+                    ДатаСоздания = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ДатаОбновления = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Анализы", x => x.Ид);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ЗаписиИнспекций",
+                columns: table => new
+                {
+                    Ид = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ИнспекцияИд = table.Column<int>(type: "int", nullable: false),
+                    НомерСерии = table.Column<int>(type: "int", nullable: false),
+                    КоличествоРастенийВСерии = table.Column<int>(type: "int", nullable: false),
+                    Карантин = table.Column<int>(type: "int", nullable: true),
+                    ВиральныеБолезни = table.Column<int>(type: "int", nullable: true),
+                    Черноножка = table.Column<int>(type: "int", nullable: true),
+                    Безтиповые = table.Column<int>(type: "int", nullable: true),
+                    Ризоктония = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ЗаписиИнспекций", x => x.Ид);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Заявки",
+                columns: table => new
+                {
+                    Ид = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ТипЗаявки = table.Column<int>(type: "int", nullable: false),
+                    НаваниеСортаСемян = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    КлассСемянИд = table.Column<int>(type: "int", nullable: false),
+                    УчастокИд = table.Column<int>(type: "int", nullable: false),
+                    РазмерКлубня = table.Column<int>(type: "int", nullable: false),
+                    КоординатыУчастка = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    СевооборотПрошлогоГода = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Севооборот2ГодаНазад = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Севооборот3ГодаНазад = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ИзоляцияПолей = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    СвидетельствоПроисхожденияСемян = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    ЗаключениеОНематодах = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Протравители = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Инсектициды = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Фунгициды = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Удобрения = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Гербициды = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ФормаУчастка = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ПлощадьПосадки = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    РасстояниеМеждуРядами = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    РасстояниеВРяду = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ПрогнозируемоеКоличествоУрожая = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ДатаПосадки = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ДатаСбора = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Активно = table.Column<bool>(type: "bit", nullable: false),
+                    Статус = table.Column<int>(type: "int", nullable: false),
+                    ДатаСоздания = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Заявки", x => x.Ид);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Инспекции",
+                columns: table => new
+                {
+                    Ид = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Статус = table.Column<int>(type: "int", nullable: false),
+                    ЗаявкаИд = table.Column<int>(type: "int", nullable: false),
+                    ТипИнспекции = table.Column<int>(type: "int", nullable: false),
+                    ПланируемаяДата = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ФактическаяДата = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ФизиологическаяСтадия = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ОбщиеУсловияПоля = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ИмяВедущегоИнспектора = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ДругиеИнспектора = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ЦелевойНачальныйПорог = table.Column<int>(type: "int", nullable: false),
+                    ПричинаОтказа = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ДатаСоздания = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Инспекции", x => x.Ид);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ПроизводителиСемян",
+                columns: table => new
+                {
+                    Ид = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ФормаХозяйствования = table.Column<int>(type: "int", nullable: false),
+                    НаваниеКомпании = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    БинИлиИин = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ЮридическийАдрес = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    КонтактноеЛицо = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ЭлектроннаяПочтаКонтактногоЛица = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    НомерТелефонаКонтактногоЛица = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Активно = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ПроизводителиСемян", x => x.Ид);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "УчасткиПолей",
+                columns: table => new
+                {
+                    Ид = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Название = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    АдресУчастка = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ПравоустанавливающиеДокументы = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Активно = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_УчасткиПолей", x => x.Ид);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Анализы");
+
+            migrationBuilder.DropTable(
+                name: "ЗаписиИнспекций");
+
+            migrationBuilder.DropTable(
+                name: "Заявки");
+
+            migrationBuilder.DropTable(
+                name: "Инспекции");
+
+            migrationBuilder.DropTable(
+                name: "ПроизводителиСемян");
+
+            migrationBuilder.DropTable(
+                name: "УчасткиПолей");
+        }
+    }
+}
--- СертификацияСемян/Данные/КонтекстБдПриложения.cs
+++ СертификацияСемян/Данные/КонтекстБдПриложения.cs
@@ -9,4 +9,122 @@ public class КонтекстБдПриложения : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<ПроизводительСемян> ПроизводителиСемян { get; set; }
+    public DbSet<Заявка> Заявки { get; set; }
+    public DbSet<УчастокПоля> УчасткиПолей { get; set; }
+    public DbSet<Инспекция> Инспекции { get; set; }
+    public DbSet<ЗаписьИнспекции> ЗаписиИнспекций { get; set; }
+    public DbSet<Анализ> Анализы { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<ПроизводительСемян>().HasKey(nameof(ПроизводительСемян.Ид));
+        builder.Entity<Заявка>().HasKey(nameof(Заявка.Ид));
+        builder.Entity<УчастокПоля>().HasKey(nameof(УчастокПоля.Ид));
+        builder.Entity<Инспекция>().HasKey(nameof(Инспекция.Ид));
+        builder.Entity<ЗаписьИнспекции>().HasKey(nameof(ЗаписьИнспекции.Ид));
+        builder.Entity<Анализ>().HasKey(nameof(Анализ.Ид));
+        base.OnModelCreating(builder);
+    }
+}
+
+public class Заявка
+{
+    public int Ид { get; set; }
+
+    public int ТипЗаявки { get; set; }
+    public string НаваниеСортаСемян { get; set; }
+
+    public int КлассСемянИд { get; set; }
+
+    public int УчастокИд { get; set; }
+
+    public int РазмерКлубня { get; set; }
+
+    public string КоординатыУчастка { get; set; }
+    public string СевооборотПрошлогоГода { get; set; }
+    public string Севооборот2ГодаНазад { get; set; }
+    public string Севооборот3ГодаНазад { get; set; }
+    public string ИзоляцияПолей { get; set; }
+    public byte[] СвидетельствоПроисхожденияСемян { get; set; }
+    public byte[] ЗаключениеОНематодах { get; set; }
+    public string Протравители { get; set; }
+    public string Инсектициды    { get; set; }
+    public string Фунгициды { get; set; }
+    public string Удобрения { get; set; }
+    public string Гербициды { get; set; }
+    public string ФормаУчастка { get; set; }
+    public string ПлощадьПосадки { get; set; }
+    public string РасстояниеМеждуРядами { get; set; }
+    public string РасстояниеВРяду { get; set; }
+    public string ПрогнозируемоеКоличествоУрожая { get; set; }
+    public DateTime ДатаПосадки { get; set; }
+    public DateTime ДатаСбора { get; set; }
+    public bool Активно { get; set; }
+    public int Статус { get; set; }
+    public DateTime ДатаСоздания { get; set; }
+}
+
+public class УчастокПоля
+{
+    public int Ид { get; set; }
+    public string Название { get; set; }
+    public string АдресУчастка { get; set; }
+    public byte[] ПравоустанавливающиеДокументы { get; set; }
+    public bool Активно { get; set; }
+}
+
+public class Инспекция
+{
+    public int Ид { get; set; }
+    public int Статус { get; set; }
+    public int ЗаявкаИд { get; set; }
+    public int ТипИнспекции { get; set; }
+    public DateTime ПланируемаяДата { get; set; }
+    public DateTime? ФактическаяДата { get; set; }
+    public string ФизиологическаяСтадия { get; set; }
+    public string ОбщиеУсловияПоля { get; set; }
+    public string ИмяВедущегоИнспектора { get; set; }
+    public string ДругиеИнспектора { get; set; }
+    public int ЦелевойНачальныйПорог { get; set; }
+    public string? ПричинаОтказа { get; set; }
+    public DateTime ДатаСоздания { get; set; }
+}
+
+public class ЗаписьИнспекции
+{
+    public int Ид { get; set; }
+    public int ИнспекцияИд { get; set; }
+    public int НомерСерии { get; set; }
+    public int КоличествоРастенийВСерии { get; set; }
+    public int? Карантин { get; set; }
+    public int? ВиральныеБолезни { get; set; }
+    public int? Черноножка { get; set; }
+    public int? Безтиповые { get; set; }
+    public int? Ризоктония { get; set; }
+}
+
+public class Анализ
+{
+    public int Ид { get; set; }
+    public int ИнспекцияИд { get; set; }
+    public int ТипАнализа { get; set; }
+    public int Статус { get; set; }
+    public DateTime ДатаСоздания { get; set; }
+    public DateTime ДатаОбновления { get; set; }
+}
+
+public class ПроизводительСемян
+{
+    public int Ид { get; set; }
+
+    public int ФормаХозяйствования { get; set; }
+    public string НаваниеКомпании { get; set; }
+    public string БинИлиИин { get; set; }
+    public string ЮридическийАдрес { get; set; }
+    public string КонтактноеЛицо { get; set; }
+    public string ЭлектроннаяПочтаКонтактногоЛица { get; set; }
+    public string НомерТелефонаКонтактногоЛица { get; set; }
+    public bool Активно { get; set; }
 }
