77a6b38e5488afadd18eaf78b034484269b011c1
Andrey K <kant2002@gmail.com>
2023-11-14 08:13:08 +0000

    Merged PR 8789: Добавил столбцы с фамилиями/именами, чтобы можно было создать инспекторов.

---
+++ СертификацияСемян/Данные/Migrations/20231114080614_ФИО.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace СертификацияСемян.Данные.Migrations
+{
+    /// <inheritdoc />
+    public partial class ФИО : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Имя",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Отчество",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Фамилия",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Имя",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Отчество",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Фамилия",
+                table: "AspNetUsers");
+        }
+    }
+}
--- СертификацияСемян/Данные/ПользовательПриложения.cs
+++ СертификацияСемян/Данные/ПользовательПриложения.cs
@@ -4,4 +4,7 @@ namespace СертификацияСемян.Данные;
 
 public class ПользовательПриложения : IdentityUser
 {
+    public string? Фамилия { get; set; }
+    public string? Имя { get; set; }
+    public string? Отчество { get; set; }
 }
