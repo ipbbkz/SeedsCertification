40a3b181253710faf6ad04eeede6f9d267760336
Andrey K <kant2002@gmail.com>
2023-11-14 09:30:46 +0000

    Merged PR 8790: Add creation of new inspector workflow

---
+++ СертификацияСемян/Данные/МодельПользователя.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace СертификацияСемян.Данные;
+
+public class МодельПользователя
+{
+    [Required]
+    [EmailAddress]
+    public string Почта { get; set; } = "";
+
+    [Required]
+    public string Фамилия { get; set; } = "";
+
+    [Required]
+    public string Имя { get; set; } = "";
+
+    public string? Отчество { get; set; }
+}
