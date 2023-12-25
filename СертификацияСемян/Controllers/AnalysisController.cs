16877cfb52554ac7049e48e52105555c41add5ea
Andrey K <kant2002@gmail.com>
2023-12-25 07:42:25 +0000

    Merged PR 8893: Add historical view of the analysis results
    Add historical view of the analysis results

---
+++ СертификацияСемян/Controllers/AnalysisController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace СертификацияСемян.Controllers;
+
+public class AnalysisController : Controller
+{
+    private readonly УправляющийИнспекциями управляющийИнспекциями;
+    public AnalysisController(УправляющийИнспекциями управляющийИнспекциями)
+    {
+        this.управляющийИнспекциями = управляющийИнспекциями ?? throw new ArgumentNullException(nameof(управляющийИнспекциями));
+    }
+
+    [HttpGet("analysis/{ид}/file")]
+    public Stream GetLabAnalysis(int ид)
+    {
+        var документы = управляющийИнспекциями.ПолучитьРезультатыАнализов(ид);
+        return new MemoryStream(документы ?? Array.Empty<byte>());
+    }
+}
--- СертификацияСемян/Данные/МодельРезультатаАнализа.cs
+++ СертификацияСемян/Данные/МодельРезультатаАнализа.cs
@@ -5,6 +5,7 @@ public class МодельРезультатаАнализа
     public int КоличествоОбразцов { get; set; }
     public DateTime ДатаВзятияОбразца { get; set; }
     public DateTime ДатаПередачиВЛабораторию { get; set; }
+    public int Статус { get; set; }
 
     [Обязательное]
     public byte[] ФайлСАнализами { get; set; }
