c7d3f0d5a5f04663eba5312742feee353fd0e1e9
Andrey K <kant2002@gmail.com>
2024-01-24 08:46:52 +0000

    Merged PR 8943: Выпуск сертификата продажи партии
    Выпуск сертификата продажи партии

---
+++ СертификацияСемян/Данные/МодельПартии.cs
@@ -0,0 +1,18 @@
+﻿namespace СертификацияСемян.Данные;
+
+public class МодельПартии
+{
+    public int Ид { get; set; }
+
+    public string КодСертификат { get; set; }
+
+    public string НазваниеСортаСемян { get; set; }
+
+    public int КлассСемянИд { get; set; }
+
+    public DateTime ДатаВыпуска { get; set; }
+
+    public int ВесПартии { get; set; }
+
+    public int Сертификат { get; set; }
+}
