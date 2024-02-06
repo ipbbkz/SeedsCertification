d1f09b02fae095a167169319dd640a832a79d904
Andrey K <kant2002@gmail.com>
2024-02-06 06:07:11 +0000

    Merged PR 8978: Цепочка происхождения
    Цепочка происхождения

---
+++ СертификацияСемян/Данные/МодельСертификатаПроисхождения.cs
@@ -0,0 +1,9 @@
+﻿namespace СертификацияСемян.Данные;
+
+public class МодельСертификатаПроисхождения
+{
+    public required string Код { get; set; }
+    public required string НазваниеКомпании { get; set; }
+    public required string СтранаПроизводитель { get; set; }
+    public required int ГодВыпуска { get; set; }
+}
